737c8254b479253ecef7315a6923f060d259b19d
morteza <mm.gov.1381@gmail.com>
2024-04-04 12:18:17 +0330

    adding viewset

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializers
 
-# Create your views here.
+
+class CategoryView(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing categories
+    """
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializers(self.queryset, many=True)
+        return Response(serializer.data)
